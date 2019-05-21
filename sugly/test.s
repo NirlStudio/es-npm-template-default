197110dc10950abaffc70c073e774468ccfe58eb
Leevi <leevi@nirlstudio.com>
2019-05-21 18:14:21 +1200

    update assertion to eq instead of is

--- sugly/test.s
+++ sugly/test.s
@@ -8,9 +8,9 @@
     assert (module resolve-path "file-name.txt":: ends-with "file-name.txt");
   ).
   (should "(module say-hello-to) returns 'Hello, World'." (=> ()
-    assert (module say-hello-to:: is "Hello, World");
+    assert "Hello, World" (module say-hello-to);
   ).
   (should "(module say-hello-to \"Tom\") returns 'Hello, Tom'." (=> ()
-    assert (module say-hello-to "Tom":: is "Hello, Tom");
+    assert "Hello, Tom" (module say-hello-to "Tom");
   ).
 ).
